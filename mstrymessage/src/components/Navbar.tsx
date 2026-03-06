3881418e9c719e60136fba02eaf43260a255cfa9
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2026-03-06 20:36:02 +0530

    navbar done

--- mstrymessage/src/components/Navbar.tsx
+++ mstrymessage/src/components/Navbar.tsx
@@ -1,7 +1,42 @@
+"use client";
 import React from "react";
-
+import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
+import { User } from "next-auth";
+import { Button } from "./ui/button";
 function Navbar() {
-  return <div>Navbar</div>;
+  const { data: session } = useSession();
+  const user: User = session?.user as User;
+  return (
+    <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <a href="#" className="text-xl font-bold mb-4 md:mb-0">
+          True Feedback
+        </a>
+        {session ? (
+          <>
+            <span className="mr-4">Welcome {user.username || user.email}</span>
+            <Button
+              onClick={() => signOut()}
+              className="w-full md:w-auto bg-slate-100 text-black"
+              variant="outline"
+            >
+              SignOut
+            </Button>
+          </>
+        ) : (
+          <Link href={`/sign-in`}>
+            <Button
+              className="w-full md:w-auto bg-slate-100 text-black rounded px-2 py-1"
+              variant={"outline"}
+            >
+              Login
+            </Button>
+          </Link>
+        )}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
